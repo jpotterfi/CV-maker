a28e532a6f15b3e9136b19fc44502f66ab137e89
jpotterfi <jeremy.potterfield@gmail.com>
2022-10-05 15:18:06 -0400

    added banner picture duo to Preview component

--- cv-project/src/Components/Preview.js
+++ cv-project/src/Components/Preview.js
@@ -3,26 +3,51 @@ import Orange from "../Banners/orange.svg";
 import Bluegrey from "../Banners/bluegrey.svg";
 import Bluered from "../Banners/bluered.svg";
 export default function Preview(props) {
-  let bannerStyle;
+  let colorScheme;
+  let bannerImage;
   if (props.data.banner === "purple") {
-    bannerStyle = Purple;
+    bannerImage = Purple;
+    colorScheme = {
+      borderColor: "#6E44FF",
+    };
   }
   if (props.data.banner === "orange") {
-    bannerStyle = Orange;
+    bannerImage = Orange;
   }
   if (props.data.banner === "bluegrey") {
-    bannerStyle = Bluegrey;
+    bannerImage = Bluegrey;
   }
   if (props.data.banner === "bluered") {
-    bannerStyle = Bluered;
+    bannerImage = Bluered;
   }
 
+  let photoStyle;
+  if (props.photoFile === "") {
+    photoStyle = {
+      backgroundColor: "white",
+    };
+  } else if (props.photoFile !== "") {
+    photoStyle = {
+      backgroundColor: "none",
+      backgroundImage: `url(${props.photoFile})`,
+    };
+  }
+
+  console.log("photoStyle", photoStyle);
+
   return (
-    <div className="column">
+    <div className="column preview__bannerduo">
       <div
         id="preview__banner"
-        style={{ backgroundImage: `url(${bannerStyle})` }}
+        style={{ backgroundImage: `url(${bannerImage})` }}
       ></div>
+      {props.data.usePhoto && (
+        <div
+          id="preview__photo"
+          className="overlay"
+          style={{ ...photoStyle, ...colorScheme }}
+        ></div>
+      )}
     </div>
   );
 }
